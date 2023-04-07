3e793c1e86924e81045055db178b735d8ea4df7b
Rei <ntptensai@gmail.com>
2023-04-07 12:23:39 +0900

    17th Commit - 'Finish Tuan06'

--- Tuan06/JS/Tuan06.js
+++ Tuan06/JS/Tuan06.js
@@ -23,10 +23,12 @@ function checkPassword(){
     if(regex.test(password)){
         text.innerHTML = "✓ Hợp lệ";
         text.style.color = "green";
+        return;
     }
     else{
         text.innerHTML = "✘ Không hợp lệ";
         text.style.color = "red";
+        return;
     }
 }
 
@@ -38,15 +40,116 @@ function recheckPassword(){
         if(password == repassword){
             text.innerHTML = "✓ Trùng";
             text.style.color = "green";
+            return;
         }
         else{
             text.innerHTML = "✘ Không trùng";
             text.style.color = "red";
+            return;
         }
     }
     else{
         text.innerHTML = "✘ Nhập mật khẩu";
         text.style.color = "red";
+        return;
+    }
+}
+
+function checkName(){
+    var regex = /^(([A-Z])[a-z]+)(( )([A-Z])[a-z]+)+$/;
+    var name = document.getElementById("nameInput").value.trim();
+    var text = document.getElementById("errName");
+    if(regex.test(name)){
+        text.innerHTML = "✓ Hợp lệ";
+        text.style.color = "green";
+        return;
+    }
+    else{
+        text.innerHTML = "✘ Tên phải có hai chữ trở lên và viết hoa đầu chữ";
+        text.style.color = "red";
+        return;
+    }
+}
+
+function checkBirthday(){
+    var birthDay = new Date(document.getElementById("dateInput").value);
+    var today = new Date();
+    var text = document.getElementById("errDate");
+    if(birthDay.getFullYear < 1900)
+    {
+        text.innerHTML = "✘ Thời gian không hợp lệ";
+        text.style.color = "red";
+        return;
+    }
+    var age = today.getFullYear() - birthDay.getFullYear();
+    if(age > 18){
+        text.innerHTML = "✓ Hợp lệ";
+        text.style.color = "green";
+        return;
+    }
+    else{
+        text.innerHTML = "✘ Chưa đủ 18 tuổi để đăng ký";
+        text.style.color = "red";
+        return;
+    }
+}
+
+function checkAddress(){
+    var address = document.getElementById("addressInput").value.trim();
+    var text = document.getElementById("errAddress");
+    if(address.length > 0){
+        text.innerHTML = "✓ Hợp lệ";
+        text.style.color = "green";
+        return;
+    }
+    else{
+        text.innerHTML = "✘ Không được để trống";
+        text.style.color = "red";
+        return;
+    }
+}
+
+function checkPhoneNumber(){
+    var regex = /^0[0-9]{9}$/;
+    var phoneNumber = document.getElementById("phoneInput").value.trim();
+    var text = document.getElementById("errPhone");
+    if(regex.test(phoneNumber)){
+        text.innerHTML = "✓ Hợp lệ";
+        text.style.color = "green";
+        return;
+    }
+    else{
+        text.innerHTML = "✘ Số điện thoại phải bắt đầu bằng số 0 và có 10 chữ số";
+        text.style.color = "red";
+        return;
+    }
+}
+
+function checkGender(){
+    var radioNam = document.getElementById("radioNam");
+    var radioNu = document.getElementById("radioNu");
+    var text = document.getElementById("errGender");
+    if(radioNam.checked || radioNu.checked){
+        text.innerHTML = "✓ Hợp lệ";
+        text.style.color = "green";
+        return;
+    }
+}
+
+function checkEmail(){
+    var regex = /^(.+)(@)([a-zA-Z]+)(\.)([a-zA-Z]+)$/;
+    var email = document.getElementById("emailInput").value.trim();
+    var text = document.getElementById("errEmail");
+    if(regex.test(email)){
+        text.innerHTML = "✓ Hợp lệ";
+        text.style.color = "green";
+        return;
+    }
+    else
+    {
+        text.innerHTML = "✘ Email phải có dạng \"abc@omn.xyz\"";
+        text.style.color = "red";
+        return;
     }
 }
 
@@ -62,4 +165,17 @@ function resetForm(){
     document.getElementById("errPw").style.color = "red";
     document.getElementById("errRePw").innerHTML = "* Bắt buộc";
     document.getElementById("errRePw").style.color = "red";
+    document.getElementById("errName").innerHTML = "* Bắt buộc";
+    document.getElementById("errName").style.color = "red";
+    document.getElementById("errDate").innerHTML = "* Bắt buộc";
+    document.getElementById("errDate").style.color = "red";
+    document.getElementById("errAddress").innerHTML = "* Bắt buộc";
+    document.getElementById("errAddress").style.color = "red";
+    document.getElementById("errPhone").innerHTML = "* Bắt buộc";
+    document.getElementById("errPhone").style.color = "red";
+    document.getElementById("errGender").innerHTML = "* Bắt buộc";
+    document.getElementById("errGender").style.color = "red";
+    document.getElementById("errEmail").innerHTML = "* Bắt buộc";
+    document.getElementById("errEmail").style.color = "red";
+    
 }
